e4f88f3c0396b4817ad8a72ab96c7e5dc0fc26b9
maxxb <maxxball09@gmail.com>
2026-02-12 22:30:41 +0300

    123

--- src/game1.java
+++ src/game1.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class game1 extends JPanel implements KeyListener {
+
     Block[] fallenBlocks = new Block[1000];
     int fallenCount = 0;
     GameObject currentObject;
@@ -15,15 +16,22 @@ public class game1 extends JPanel implements KeyListener {
     int happyTickCounter = 0;
     boolean happyFastMode = false;
     Font customFont;
+
     int[] typeDelayCounter = new int[4];
     int[] typeDelayThreshold = {2, 1, 3};
+
+    int rotationDelayCounter = 0;
+    int rotationDelayThreshold = 5;
+
     boolean animatingDrop = false;
     int animationStep = 0;
     final int ANIMATION_STEPS = 15;
+
     int[] animatingBlocksIndices = new int[1000];
     int[] animationStartY = new int[1000];
     int[] animationTargetY = new int[1000];
     int animatingBlocksCount = 0;
+
     int[][][] figures = {
             {{0,0}, {1,0}, {2,0}},
             {{0,0}, {0,1}, {0,2}},
@@ -33,8 +41,10 @@ public class game1 extends JPanel implements KeyListener {
             {{0,0}, {-1,1}, {0,1}, {1,1}},
             {{0,0}, {1,0}, {-1,1}, {0,1}, {1,1}}
     };
+
     int gridWidth = 16;
     int gridHeight = 10;
+
     public game1(JFrame parent) {
         this.parentFrame = parent;
         setPreferredSize(new Dimension(1200, 900));
@@ -46,8 +56,10 @@ public class game1 extends JPanel implements KeyListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (!gameActive) return;
+
                 if (animatingDrop) {
                     animationStep++;
+
                     for (int i = 0; i < animatingBlocksCount; i++) {
                         int blockIndex = animatingBlocksIndices[i];
                         if (fallenBlocks[blockIndex] != null) {
@@ -57,6 +69,7 @@ public class game1 extends JPanel implements KeyListener {
                             fallenBlocks[blockIndex].y = newY;
                         }
                     }
+
                     if (animationStep >= ANIMATION_STEPS) {
                         for (int i = 0; i < animatingBlocksCount; i++) {
                             int blockIndex = animatingBlocksIndices[i];
@@ -65,14 +78,26 @@ public class game1 extends JPanel implements KeyListener {
                                 fallenBlocks[blockIndex].gridY = (fallenBlocks[blockIndex].y - 50) / setka_razm;
                             }
                         }
+
                         animatingDrop = false;
                         animationStep = 0;
                         animatingBlocksCount = 0;
+
                         checkFullLines();
                     }
+
                     repaint();
                     return;
                 }
+
+                rotationDelayCounter++;
+                if (rotationDelayCounter >= rotationDelayThreshold) {
+                    rotationDelayCounter = 0;
+                    if (canRotate() && !animatingDrop) {
+                        rotateObject();
+                    }
+                }
+
                 if (currentObject.type == 3) {
                     happyTickCounter++;
                     if (happyTickCounter >= 7) {
@@ -80,13 +105,16 @@ public class game1 extends JPanel implements KeyListener {
                         happyTickCounter = 0;
                     }
                 }
+
                 typeDelayCounter[currentObject.type]++;
                 int requiredDelay = typeDelayThreshold[currentObject.type - 1];
                 if (happyFastMode && currentObject.type == 3) {
                     requiredDelay = 1;
                 }
+
                 if (typeDelayCounter[currentObject.type] >= requiredDelay) {
                     typeDelayCounter[currentObject.type] = 0;
+
                     if (!moveDown()) {
                         freezeObject();
                         checkFullLines();
@@ -102,25 +130,92 @@ public class game1 extends JPanel implements KeyListener {
         });
         gameTimer.start();
     }
+
     private boolean moveDown() {
         if (currentObject == null) return false;
+
         for (Block block : currentObject.blocks) {
             int newGridY = block.gridY + 1;
+
             if (newGridY >= gridHeight) {
                 return false;
             }
-            if (zanyata_li_yacheika(block.gridX, newGridY)) {
+
+            if (isCellOccupied(block.gridX, newGridY)) {
                 return false;
             }
         }
+
         for (Block block : currentObject.blocks) {
             block.gridY++;
             block.y = 50 + block.gridY * setka_razm;
         }
         currentObject.objY += setka_razm;
+
         return true;
     }
-    private boolean zanyata_li_yacheika(int gridX, int gridY) {
+
+    private boolean canRotate() {
+        if (currentObject == null || animatingDrop) return false;
+
+        int[][] rotated = rotateShape(currentObject.obj);
+
+        int baseGridX = currentObject.blocks[0].gridX - currentObject.obj[0][0];
+        int baseGridY = currentObject.blocks[0].gridY - currentObject.obj[0][1];
+
+        for (int i = 0; i < rotated.length; i++) {
+            int newGridX = baseGridX + rotated[i][0];
+            int newGridY = baseGridY + rotated[i][1];
+
+            if (newGridX < 0 || newGridX >= gridWidth || newGridY < 0 || newGridY >= gridHeight) {
+                return false;
+            }
+
+            if (isCellOccupied(newGridX, newGridY)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int[][] rotateShape(int[][] shape) {
+        int[][] rotated = new int[shape.length][2];
+
+        for (int i = 0; i < shape.length; i++) {
+            int x = shape[i][0];
+            int y = shape[i][1];
+            rotated[i][0] = -y;
+            rotated[i][1] = x;
+        }
+
+        return rotated;
+    }
+
+    private void rotateObject() {
+        if (!canRotate()) return;
+
+        int[][] rotated = rotateShape(currentObject.obj);
+
+        int baseGridX = currentObject.blocks[0].gridX - currentObject.obj[0][0];
+        int baseGridY = currentObject.blocks[0].gridY - currentObject.obj[0][1];
+
+        currentObject.obj = rotated;
+
+        for (int i = 0; i < currentObject.blocks.length; i++) {
+            int newGridX = baseGridX + rotated[i][0];
+            int newGridY = baseGridY + rotated[i][1];
+
+            currentObject.blocks[i].gridX = newGridX;
+            currentObject.blocks[i].gridY = newGridY;
+            currentObject.blocks[i].x = newGridX * setka_razm;
+            currentObject.blocks[i].y = 50 + newGridY * setka_razm;
+        }
+
+        repaint();
+    }
+
+    private boolean isCellOccupied(int gridX, int gridY) {
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null &&
                     fallenBlocks[i].gridX == gridX &&
@@ -130,9 +225,10 @@ public class game1 extends JPanel implements KeyListener {
         }
         return false;
     }
+
     private void freezeObject() {
         for (Block block : currentObject.blocks) {
-            if (!zanyata_li_yacheika(block.gridX, block.gridY)) {
+            if (!isCellOccupied(block.gridX, block.gridY)) {
                 if (fallenCount < fallenBlocks.length) {
                     fallenBlocks[fallenCount] = new Block(
                             block.x, block.y,
@@ -143,56 +239,68 @@ public class game1 extends JPanel implements KeyListener {
                 }
             }
         }
+
         for (int i = 0; i < typeDelayCounter.length; i++) {
             typeDelayCounter[i] = 0;
         }
     }
+
     private void checkFullLines() {
         boolean lineRemoved = true;
         while (lineRemoved) {
             lineRemoved = false;
+
             for (int gridY = gridHeight - 1; gridY >= 0; gridY--) {
-                if (ryadom_bloki(gridY, 7)) {
-                    udalit_Line(gridY);
+                if (hasBlocksInLine(gridY, 7)) {
+                    removeLineWithAnimation(gridY);
                     lineRemoved = true;
                     break;
                 }
             }
         }
     }
-    private void udalit_Line(int removedGridY) {
+
+    private void removeLineWithAnimation(int removedGridY) {
         boolean[] lineToRemove = new boolean[fallenCount];
+
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null && fallenBlocks[i].gridY == removedGridY) {
                 lineToRemove[i] = true;
             }
         }
+
         for (int i = 0; i < fallenCount; i++) {
             if (lineToRemove[i]) {
                 fallenBlocks[i] = null;
             }
         }
+
         compressArray();
+
         animatingBlocksCount = 0;
+
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null && fallenBlocks[i].gridY < removedGridY) {
                 int startY = fallenBlocks[i].y;
                 int targetY = 50 + (fallenBlocks[i].gridY + 1) * setka_razm;
+
                 animatingBlocksIndices[animatingBlocksCount] = i;
                 animationStartY[animatingBlocksCount] = startY;
                 animationTargetY[animatingBlocksCount] = targetY;
                 animatingBlocksCount++;
+
                 fallenBlocks[i].gridY++;
             }
         }
+
         if (animatingBlocksCount > 0) {
             animatingDrop = true;
             animationStep = 0;
         }
     }
-    private boolean ryadom_bloki(int gridY, int minConsecutive) {
-        boolean[] lineCells = new boolean[gridWidth];
 
+    private boolean hasBlocksInLine(int gridY, int minConsecutive) {
+        boolean[] lineCells = new boolean[gridWidth];
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null && fallenBlocks[i].gridY == gridY) {
                 if (fallenBlocks[i].gridX >= 0 && fallenBlocks[i].gridX < gridWidth) {
@@ -200,6 +308,7 @@ public class game1 extends JPanel implements KeyListener {
                 }
             }
         }
+
         int maxConsecutive = 0;
         int currentConsecutive = 0;
 
@@ -216,18 +325,22 @@ public class game1 extends JPanel implements KeyListener {
 
         return maxConsecutive >= minConsecutive;
     }
+
     private void compressArray() {
         int newIndex = 0;
         Block[] temp = new Block[fallenBlocks.length];
+
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null) {
                 temp[newIndex] = fallenBlocks[i];
                 newIndex++;
             }
         }
+
         fallenBlocks = temp;
         fallenCount = newIndex;
     }
+
     private boolean isGameOver() {
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null && fallenBlocks[i].gridY < 1) {
@@ -244,19 +357,24 @@ public class game1 extends JPanel implements KeyListener {
                 "Игра окончена!\nСобрано блоков: " + fallenCount,
                 "Конец игры", JOptionPane.INFORMATION_MESSAGE);
     }
+
     private void createNewObject() {
         int[][] shape = figures[rand.nextInt(figures.length)];
         int type = rand.nextInt(3) + 1;
+
         int startGridX = getAlignedGridX();
         int startGridY = 0;
+
         Block[] blocks = new Block[shape.length];
         for (int i = 0; i < shape.length; i++) {
             int gridX = startGridX + shape[i][0];
             int gridY = startGridY + shape[i][1];
             int pixelX = gridX * setka_razm;
             int pixelY = 50 + gridY * setka_razm;
+
             blocks[i] = new Block(pixelX, pixelY, gridX, gridY, type);
         }
+
         currentObject = new GameObject(
                 startGridX * setka_razm,
                 50 + startGridY * setka_razm,
@@ -264,31 +382,42 @@ public class game1 extends JPanel implements KeyListener {
                 type,
                 blocks
         );
+
         if (type == 3) {
             happyTickCounter = 0;
             happyFastMode = false;
         }
+
         typeDelayCounter[type] = 0;
+        rotationDelayCounter = 0;
+
         if (isGameOver()) {
             showGameOver();
         }
     }
+
     private int getAlignedGridX() {
         return rand.nextInt(gridWidth - 4);
     }
+
     private boolean canMove(int deltaGridX) {
         if (!gameActive || currentObject == null || animatingDrop) return false;
+
         for (Block block : currentObject.blocks) {
             int newGridX = block.gridX + deltaGridX;
+
             if (newGridX < 0 || newGridX >= gridWidth) {
                 return false;
             }
-            if (zanyata_li_yacheika(newGridX, block.gridY)) {
+
+            if (isCellOccupied(newGridX, block.gridY)) {
                 return false;
             }
         }
+
         return true;
     }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -299,14 +428,17 @@ public class game1 extends JPanel implements KeyListener {
             g.setColor(new Color(20, 20, 30));
             g.fillRect(0, 0, 1200, 900);
         }
+
         for (int i = 0; i < fallenCount; i++) {
             if (fallenBlocks[i] != null) {
                 drawBlock(g, fallenBlocks[i]);
             }
         }
+
         if (currentObject != null && !animatingDrop) {
             drawObject(g, currentObject);
         }
+
         g.setColor(new Color(255, 255, 255, 200));
         for (int x = 0; x <= 1200; x += setka_razm) {
             g.drawLine(x, 50, x, 900);
@@ -314,8 +446,10 @@ public class game1 extends JPanel implements KeyListener {
         for (int y = 50; y <= 900; y += setka_razm) {
             g.drawLine(0, y, 1200, y);
         }
+
         g.setColor(new Color(255, 255, 0, 200));
         g.drawRect(0, 50, 1200, 850);
+
         g.setColor(Color.WHITE);
         g.setFont(customFont);
         g.drawString("Фигур: " + (fallenCount / 3), 1050, 40);
@@ -367,6 +501,9 @@ public class game1 extends JPanel implements KeyListener {
             deltaGridX = -1;
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             deltaGridX = 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            rotateObject();
+            return;
         }
 
         if (deltaGridX != 0 && canMove(deltaGridX)) {
@@ -386,7 +523,7 @@ public class game1 extends JPanel implements KeyListener {
     public void keyTyped(KeyEvent e) {}
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
+        JFrame f = new JFrame("Эмоциональный Тетрис");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(1200, 900);
         f.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -395,12 +532,14 @@ public class game1 extends JPanel implements KeyListener {
         f.setVisible(true);
         panel.requestFocusInWindow();
     }
+
     class GameObject {
         int objX;
         int objY;
         int[][] obj;
         int type;
         Block[] blocks;
+
         GameObject(int objX, int objY, int[][] obj, int type, Block[] blocks) {
             this.objX = objX;
             this.objY = objY;
@@ -409,10 +548,12 @@ public class game1 extends JPanel implements KeyListener {
             this.blocks = blocks;
         }
     }
+
     class Block {
         int x, y;
         int gridX, gridY;
         int type;
+
         Block(int x, int y, int gridX, int gridY, int type) {
             this.x = x;
             this.y = y;
